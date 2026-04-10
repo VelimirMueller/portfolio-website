2faba93452b0c7bed77591dfccae16d51250d57d
Velimir Mueller <velimir.mueller@galvany.de>
2026-04-10 16:19:35 +0200

    feat: add BreadcrumbList JSON-LD to service detail pages

--- src/app/[locale]/(main)/services/[serviceId]/page.tsx
+++ src/app/[locale]/(main)/services/[serviceId]/page.tsx
@@ -41,5 +41,26 @@ export default async function ServiceDetailPage({ params }: Props) {
   }
 
   setRequestLocale(locale);
-  return <ServiceDetailContent serviceId={serviceId} />;
+  const t = await getTranslations({ locale, namespace: 'serviceDetail' });
+  const serviceName = t(`${serviceId}.title`);
+
+  const breadcrumbSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: [
+      { '@type': 'ListItem', position: 1, name: 'Home', item: `https://www.velimir-mueller.de/${locale}` },
+      { '@type': 'ListItem', position: 2, name: 'Services', item: `https://www.velimir-mueller.de/${locale}/services` },
+      { '@type': 'ListItem', position: 3, name: serviceName },
+    ],
+  };
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbSchema) }}
+      />
+      <ServiceDetailContent serviceId={serviceId} />
+    </>
+  );
 }
